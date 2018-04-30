571c44b811c477ee5bea6078fdc72eb424e76389
North Creative Design <info@northcreativedesign.com>
2018-04-29 21:49:41 -0800

    added new routes

--- app.js
+++ app.js
@@ -13,8 +13,16 @@ app.get("/", function(req, res){
   res.render("landing", {title: 'Alaska Traveler'});
 });
 
-app.get("/alaskainterior", function(req, res){
-  res.render("interior", {title: 'Alaska Traveler | Alaska Interior Region'});
+app.get("/anchorage-area", function(req, res){
+  res.render("anchorage_area", {title: 'Alaska Traveler | Anchorage Area'});
+});
+
+app.get("/ketchikan-area", function(req, res){
+  res.render("ketchikan_area", {title: 'Alaska Traveler | Ketchikan Area'});
+});
+
+app.get("/juneau-area", function(req, res){
+  res.render("juneau_area", {title: 'Alaska Traveler | Juneau Area'});
 });
 
 app.listen(process.env.PORT || 8000, function(){
